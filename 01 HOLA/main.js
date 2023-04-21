967ee1304a9de6f5204d85207d0516a33855fd9e
maiky <hgael083@gmail.com>
2023-04-21 10:38:27 -0500

    hola

---
+++ 01 HOLA/main.js
@@ -0,0 +1,11 @@
+const hola=()=>{
+    alert("HOLA MUNDO");
+    let name = prompt("¿cual es tu nombre?");
+    alert("hola "+name+"🔥");
+  }
+  const suma=()=>{
+    let numero1=parseInt(prompt("numero 1: "));
+  let numero2=parseInt(prompt("numero 2: "));
+  let suma= numero1 + numero2;
+  alert("Suma:" + suma);
+  }
